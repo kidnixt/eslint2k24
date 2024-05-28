4d44051219928f11103638d3f21527ea79be9318
kidnixt <kidnixt@gmail.com>
2024-05-28 15:09:48 -0300

    solucionado????

---
+++ main.js
@@ -0,0 +1,8 @@
+function mifuncion(param1, param2) {
+    // Código sin comentarios ni documentación
+  if (param1 === 0) {
+      return "El primer parámetro es cero"
+    }
+  }
+  
+  console.log(mifuncion(1, 2));
